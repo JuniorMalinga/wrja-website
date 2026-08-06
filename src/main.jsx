87da2f0dade7f116ac18b28479a71da55c867925
JuniorMalinga <ST10438896@Vcconnect.edu.za>
2026-08-06 12:19:22 +0200

    Broke up the style.css into multiple files for ease of use - some elements broke in the process

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./styles.css";
+import "./styles/base.css";
+import "./styles/home.css";
+import "./styles/programs.css";
+import "./styles/events.css";
+import "./styles/news.css";
+import "./styles/about.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
